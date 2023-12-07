885710b0214477a45e60f0a6ac3cfecce583b9ad
FahadAlHossain <fahadalhossain5@gmail.com>
2023-12-07 21:00:58 +0600

    change

--- admin/inc/header.php
+++ admin/inc/header.php
@@ -15,7 +15,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Adminpanel - Online Voting System</title>
+    <title>Adminpanel - Online Voting System UIU</title>
     <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="../assets/css/style.css">
 </head>
